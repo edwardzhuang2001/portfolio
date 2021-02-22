44213131efce31749a0df7d5d155cf291a4ff1a8
edwardzhuang2001 <45517287+edwardzhuang2001@users.noreply.github.com>
2021-02-22 11:08:31 -0500

    Added portrait properly

--- script.js
+++
@@ -1,8 +0,0 @@
-let padding = $("nav").height() + "px";
-
-document.querySelector(".navbar-toggler").onclick = function () {
-    setTimeout(function () {
-        let padding = document.querySelector("nav").offsetHeight + "px";
-        document.body.style.paddingTop = padding;
-    }, 500)
-};
