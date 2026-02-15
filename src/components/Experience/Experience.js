72c70189d2a01863e33b6f6b6b077e414e2d1b72
Edward Zhuang <edwardzhuang2001@gmail.com>
2026-02-15 00:54:43 -0500

    Update content for 1.5 years of work experience at RBC

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -14,11 +14,15 @@ function Experience() {
       <ExperienceItem
         title="Software Engineer"
         company="Royal Bank of Canada"
-        team="Anti-Money Laundering"
+        team="Anti-Money Laundering (AML)"
         date="August 2024 &ndash; Present"
         logo={rbcLogo}
         descriptions={[
-          "Coming soon!",
+          "Enabled AML regulatory coverage for a newly acquired wealth management firm by integrating client and transaction data for 70K+ clients representing $80B+ in assets into RBC’s Hadoop AML data lake, ensuring continuous screening, transaction monitoring, and regulatory compliance post‑acquisition",
+          "Implemented Spark SQL ETL pipelines and data validation frameworks to consolidate daily heterogeneous client and transaction datasets, preventing monitoring gaps that could expose the bank to undetected financial crime and regulatory risk",
+          "Modernized payment monitoring logic through SWIFT MX adoption, building a new data model (central SQL view and auxiliary tables) that improved data completeness and reduced false‑positive AML alerts by up to 30% compared to legacy formats",
+          "Drove RBC’s Hive‑to‑Snowflake AML platform migration by developing a configurable Java Spring Boot data‑flattening service to transform unstructured payment payloads (JSON, Avro) from AWS S3 into Snowflake‑ready schemas, improving downstream AML investigator query performance by 10x+",
+          "Built and maintained core components of a Scala/Java data integration platform supporting standardized ingestion and transformation across multiple data formats, enabling delivery of 10+ AML ETL initiatives with consistent controls and reduced onboarding time for new data sources"
         ]}
       />
       <ExperienceItem
@@ -28,10 +32,9 @@ function Experience() {
         date="May 2022 &ndash; April 2023"
         logo={nbcLogo}
         descriptions={[
-          "Developed and tested microservices for a securities trading software using Java and Spring Boot, ensuring the successful implementation of 100+ new features for National Bank traders",
-          "Implemented server-side infrastructure to migrate the on-site monolith to AWS, adopting microservices",
-          "Designed and built new modules for tracking daily trade notional by constructing new SQL tables and developing repository and service layer foundations to connect with the database",
-          "Performed support tasks such as maintaining Jenkins pipelines, aggregating trade data, and fixing bugs",
+          "Developed microservices for a securities trading platform in Java Spring Boot, delivering 100+ production features",
+          "Implemented server‑side infrastructure to enable the migration of the on‑premises, monolithic application to AWS, adopting a microservices architecture using Java Spring Boot, improving scalability and achieving 99.9%+ system uptime",
+          "Designed modules for daily notional value tracking by constructing new SQL tables and implementing Spring Data JPA repositories in Java, generating trade activity insights and reducing manual effort by 20+ hrs/week across 50+ traders",
         ]}
       />
       <ExperienceItem
@@ -41,7 +44,7 @@ function Experience() {
         date="May 2021 &ndash; September 2021"
         logo={ontarioMinistryOfEducationLogo}
         descriptions={[
-          "Streamlined structure creation process for the new Ontario curriculum website by producing a JavaScript script with Node.js and leveraging the Kentico Kontent API, speeding up the process by 95%",
+          "Streamlined structure creation process for the new Ontario curriculum website by leveraging JavaScript and Node.js with the Kentico Kontent API, speeding up the process by 95%+",
           "Expanded the Ontario curriculum website by using Kentico CMS to publish curricula, originally formatted as PDFs, overall enhancing the user experience",
           "Designed the Curriculum and Resources help page and additional help resources to assist users with difficulty navigating the site",
         ]}
